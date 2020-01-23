d11ca51fc6b1ead7bda627bcac8265ad61f040a6
Mork <github@morkeltry.33mail.com>
2020-01-23 12:39:42 +0000

    index.js app name matches Android bits; added android folder

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { AppRegistry, Platform } from 'react-native';
-import App from './App';
+import App from './src/App';
 
-AppRegistry.registerComponent('cleanInstallTest', () => App);
+AppRegistry.registerComponent('ting', () => App);
 
 if (Platform.OS === 'web') {
   const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('cleanInstallTest', { rootTag });
+  AppRegistry.runApplication('ting', { rootTag });
 }
